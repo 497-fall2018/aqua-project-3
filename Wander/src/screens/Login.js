2fde092c8643a3d8cdf549efca8bc280e0368115
James Xie <jamesxie2019@u.northwestern.edu>
2018-11-24 13:09:46 -0600

    login screen rendered

---
+++ Wander/src/screens/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class LoginScreen extends Component {
+    render(){
+        return(
+            <View>
+                <Text>Login Screen rendered</Text>
+            </View>
+        )
+    }
+}
+
+export default LoginScreen;
