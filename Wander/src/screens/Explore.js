f743af73fc645def44a3bfd69bbccb8423bc5d27
James Xie <jamesxie2019@u.northwestern.edu>
2018-11-27 23:31:59 -0600

    working, but setState in Explore.componentDidMount is broken

--- Wander/src/screens/Explore.js
+++ Wander/src/screens/Explore.js
@@ -12,8 +12,9 @@ import {
 import DealItem from '../components/DealItem';
 import AdventurePanel from '../components/AdventurePanel';
 
-
+// --------------------------------------------------------------------
 // ------------------ EXPLORE SCREEN LIST GENERATORS ------------------
+// --------------------------------------------------------------------
 
 const AdventureList = (props) => {
     return (
@@ -25,159 +26,195 @@ const AdventureList = (props) => {
 }
 
 const DealList = (props) => {
-    return (
-    props.deals.map((deal) => (
-        <DealItem 
-            title={deal.title}
-            time={deal.time}
-            people={deal.people}
-            key={deal.title}/>
-    )));
+    switch(props.adventure) {
+        case('Biking'):
+            return(
+                props.deals.Biking.map((deal) => (
+                    <DealItem 
+                        title={deal.title}
+                        time={deal.time}
+                        people={deal.people}
+                        key={deal.title}/>)))
+        default:
+            return (
+                props.deals.Biking.map((deal) => (
+                    <DealItem 
+                        title={deal.title}
+                        time={deal.time}
+                        people={deal.people}
+                        key={deal.title}/>)));
+    }
 }
 
+// ----------------------------------------------------
 // ------------------ EXPLORE SCREEN ------------------
+// ----------------------------------------------------
 
 class ExploreScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-        adventures: [
-            {title: 'Biking'},
-            {title: 'Skiing'},
-            {title: 'Kayaking'},
-            {title: 'Snorkeling'},
-            {title: 'Camping'},
-        ],
-        deals: [
-            {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
-            {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
-            {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
-            {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
-            {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
-            {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
-            {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
-        ]
+    constructor(props) {
+        super(props);
+        this.state={
+            adventures: [
+                {title: 'Biking'},
+                {title: 'Skiing'},
+                {title: 'Kayaking'},
+                {title: 'Snorkeling'},
+                {title: 'Camping'},
+            ],
+            currAdventure: 'ya did something wrong bitch',
+            deals: {
+                Biking: 
+                [{title: 'Expedition in New Zealand', time: '1 day', people: '6'},
+                {title: 'Expedition in New Hampshire', time: '1 day', people: '6'},
+                {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
+                {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
+                {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
+                {title: 'Expedition in New Zealand', time: '1 day', people: '6'},
+                {title: 'Expedition in New Zealand', time: '1 day', people: '6'}],
+                Skiing:
+                [{title: 'Skiing in Colorado', time: '3 days', people: '4'},
+                {title: 'Skiing in Colorado', time: '3 days', people: '4'},
+                {title: 'Skiing in Colorado', time: '3 days', people: '4'},
+                {title: 'Skiing in Colorado', time: '3 days', people: '4'},
+                {title: 'Skiing in Colorado', time: '3 days', people: '4'}],
+                Kayaking:
+                [{title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},
+                {title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},
+                {title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},
+                {title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},
+                {title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},
+                {title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},
+                {title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},
+                {title: 'Kayaking in Puerto Rico', time: '1 day', people: '4'},],
+            }
+        }
     };
-  }
 
-  onPressContinue = () => {
-    Navigation.push(this.props.componentId, {
-        component: {
-            name: 'Wander.Confirm',
-            passProps: {
-              activityTitle: 'Guided Backpacking Trek'
-            },
-            options:{
-                topBar:{
-                    visible: true,
-                    title: {
-                        text: 'Explore',
-                        fontSize: 18,
+    onPressContinue = () => {
+        Navigation.push(this.props.componentId, {
+            component: {
+                name: 'Wander.Confirm',
+                passProps: {
+                activityTitle: 'Guided Backpacking Trek'
+                },
+                options:{
+                    topBar:{
+                        visible: true,
+                        title: {
+                            text: 'Explore',
+                            fontSize: 18,
+                        }
                     }
                 }
             }
+        })
+    }
+
+    // for some reason this isn't setting currAdventure correctly
+    componentDidMount(){
+        this.setState(
+            { currAdventure: 'Biking' }
+        )
+    }
+
+    render() {
+        return (
+            <ScrollView
+                stickyHeaderIndices={[1]}
+                showsVerticalScrollIndicator={false}
+                style={{marginTop: 20}}>
+                <View style={styles.container}>
+                    <TouchableHighlight
+                    style={styles.continue}
+                    onPress={this.onPressContinue}
+                    underlayColor={'white'}>
+                        <View style={styles.continueImage}>
+                            <Image 
+                                source={require('../assets/screens/Explore/Explore1.jpeg')} 
+                                style={{width: 470, height: 210}}/>
+                            <Text style={styles.continueText}>Continue where you left off >></Text>
+                        </View>
+                    </TouchableHighlight>
+                </View>
+                <View style={{backgroundColor: 'white'}}>                 
+                    <ScrollView
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        decelerationRate={'fast'}>
+                        <View style={styles.activities}>
+                            <AdventureList
+                                adventures={this.state.adventures} />
+                        </View>
+                    </ScrollView>
+                </View>
+                <View style={deals.container}>
+                    <View>
+                        <Text style={deals.title}>Biking Adventures: </Text>
+                    </View>
+                    <DealList 
+                        deals={this.state.deals}
+                        adventure='Biking'/>
+                </View>
+            </ScrollView>
+        )
+    }
+    }
+
+    export default ExploreScreen;
+
+    const deals = StyleSheet.create({
+        container: {
+            width: '100%',
+        },
+        title: {
+            fontWeight: 'bold',
+            marginLeft: 10
+        },
+        itemContainer: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            
+        },
+        itemImage: {
+            width: 320,
+            height: 50,
+            opacity: .2,
+            borderRadius: 5,
+        },
+        itemLabel: {
+            position: 'absolute',
+            top: 20,
+            left: 160,
+            color: 'black',
+            fontWeight: 'bold',
         }
     })
-  }
 
- render() {
-    return (
-        <ScrollView
-            stickyHeaderIndices={[1]}
-            showsVerticalScrollIndicator={false}
-            style={{marginTop: 20}}>
-            <View style={styles.container}>
-                <TouchableHighlight
-                style={styles.continue}
-                onPress={this.onPressContinue}
-                underlayColor={'white'}>
-                    <View style={styles.continueImage}>
-                        <Image 
-                            source={require('../assets/screens/Explore/Explore1.jpeg')} 
-                            style={{width: 470, height: 210}}/>
-                        <Text style={styles.continueText}>Continue where you left off >></Text>
-                    </View>
-                </TouchableHighlight>
-            </View>
-            <View style={{backgroundColor: 'white'}}>                 
-            <ScrollView
-                horizontal={true}
-                showsHorizontalScrollIndicator={false}
-                decelerationRate={'fast'}>
-                <View style={styles.activities}>
-                    <AdventureList
-                        adventures={this.state.adventures} />
-                </View>
-                
-            </ScrollView>
-            </View>
-            <View style={deals.container}>
-                <View>
-                    <Text style={deals.title}>Biking Adventures: </Text>
-                </View>
-                <DealList 
-                    deals={this.state.deals}/>
-            </View>
-        </ScrollView>
-    )
-  }
-}
-
-export default ExploreScreen;
-
-const deals = StyleSheet.create({
+    const styles = StyleSheet.create({
     container: {
-        width: '100%',
-    },
-    title: {
-        fontWeight: 'bold',
-        marginLeft: 10
-    },
-    itemContainer: {
-        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        
     },
-    itemImage: {
-        width: 320,
-        height: 50,
-        opacity: .2,
-        borderRadius: 5,
+    continue: {
+        flex: .7,
     },
-    itemLabel: {
+    continueImage: {
+        overflow: 'hidden',
+        width: 380,
+    },
+    continueText: {
         position: 'absolute',
-        top: 20,
-        left: 160,
-        color: 'black',
-        fontWeight: 'bold',
-    }
-})
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-  },
-  continue: {
-    flex: .7,
-  },
-  continueImage: {
-      overflow: 'hidden',
-      width: 380,
-  },
-  continueText: {
-    position: 'absolute',
-    top: 180,
-    left: 170,
-    color: 'white',
-    fontStyle: 'italic',
-    fontWeight: '100',
-  },
-  activities: {
-    flex: .5,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    paddingBottom: 15,
-    marginRight: 10,
-  },
+        top: 180,
+        left: 170,
+        color: 'white',
+        fontStyle: 'italic',
+        fontWeight: '100',
+    },
+    activities: {
+        flex: .5,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        paddingBottom: 15,
+        marginRight: 10,
+    },
 })
